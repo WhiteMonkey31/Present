f3e317cdffec4460e05dcf803bd88611e50458ee
Saif-ur-Rehman Awan <saifurrehmanawan85@gmail.com>
2024-12-06 17:28:22 +0500

    Update demo.cppcacbhsvhjecs

--- demo.cpp
+++ demo.cpp
@@ -1,7 +1,7 @@
-#include<iostream>
-#include<stdlib.h>
-
-int main(){
-    cout<<"Enter i";
+#include<iostream>sbdVHbdsbvsdv
+#include<stdlib.h>dbntf
+n
+int mainzn(gyzfngyzng){
+ gf n gy cout<<"Enterngzyngnjygjng i";
     cout<<"i am in branch 2";
 }
